d9a2ab628d20c7000feb35fb6b3f23cf12376e6e
Youngsung Choi <67312562+yeoung004@users.noreply.github.com>
2022-05-20 21:06:56 +0900

    Delete Components/AsyncKeys directory

--- Components/AsyncKeys/ChattingKey.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    CHATTING_LIST_KEY: '@chatting_list',
-    CHATTING_ERROR_LIST_KEY: '@chatting_error_list',
-    DISTURB_LIST_KEY: '@non_disturb_list',
-    DISTURB_KEY: '@non_disturb',
-    BLOCK_LIST_KEY: '@block_list'
-}
--- Components/AsyncKeys/userKey.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    TUTORIAL_KEY : '@tutorial',
-    LANGUAGE_KEY : '@language',
-    INVITE_KEY : '@invite_list',
-    NOTIFICATION_KEY : '@notification_list',
-    POST_SEARCH_HISTORY_KEY : '@search_history'
-}
